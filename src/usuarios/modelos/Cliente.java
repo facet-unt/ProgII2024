1a370f337eab9d1ef41be7b141856898f24a1816
Juan Pablo <juampirearte2@hotmail.com>
2024-09-10 16:16:47 -0300

    Nueva rama G02, primer commit

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -1,6 +1,13 @@
-
 package usuarios.modelos;
 
 public class Cliente {
-    
+
+    String correo;
+    String clave;
+    String apellido;
+    String nombre;
+
+    public void mostrar() {
+        System.out.println(" " + correo + " " + clave + " " + apellido + " " + nombre);
+    }
 }
