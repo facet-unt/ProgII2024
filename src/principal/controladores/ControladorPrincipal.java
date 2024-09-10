dbbb579673aea7bdf5a69158147f7d82f28eec50
Nicolas Arnedo <nicolasarnedo140@gmail.com>
2024-09-10 17:18:38 -0300

    Modificacion del Producto.java y creacion de la clase ControladorPrincipal

---
+++ src/principal/controladores/ControladorPrincipal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal.controladores;
+
+import java.util.ArrayList;
+import productos.modelos.Producto;
+import usuarios.modelos.Cliente;
+import usuarios.modelos.Empleado;
+import usuarios.modelos.Encargado;
+
+/**
+ *
+ * @author estudiante
+ */
+public class ControladorPrincipal {
+    public static void main(String[] args) {
+        ArrayList<Cliente> ListaClientes = new ArrayList<>();
+        ArrayList<Empleado> ListaEmpleados = new ArrayList<>();
+        ArrayList<Encargado> ListaEncargados = new ArrayList<>();
+        ArrayList<Producto> ListaProductos = new ArrayList<>();
+        
+        
+    }
+    
+}
--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -3,19 +3,20 @@ package productos.modelos;
 import usuarios.modelos.Cliente;
 
 public class Producto {
-    public String nombre;
+    public String codigo;
     public String descripcion;
     public float precio;
     public String estado;
+    public String categoria;
 
     /**
      * Este método permite mostrar un Producto
      * @return devuelve nada
      */
-    public void mostrar() {        
-        System.out.println(nombre);
-        System.out.println(precio);        
+    void mostrar(){
+    System.out.println("Codigo: "+ codigo + "Descripcion: "+ descripcion + "Categoria: "+ categoria + "Estado: "+ estado + "Precio: "+ precio);
     }
+    
        
 }
 
