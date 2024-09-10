cacfee44245dfb72a2ebc7a99d886a88d3dad4dd
Diego Galvan <diegosebastiangalvan2018@gmail.com>
2024-09-10 17:56:43 -0300

    Diego Galvan: Agregado de las clases Cliente, Empleado y Encargado, con sus correspondientes atributos y metodo mostrar

--- src/usuarios/modelos/Empleado.java
+++ src/usuarios/modelos/Empleado.java
@@ -1,7 +1,14 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package usuarios.modelos;
 
-public class Cliente {
+/**
+ *
+ * @author estudiante
+ */
+public class Empleado {
     
     String correo;
     String clave;
