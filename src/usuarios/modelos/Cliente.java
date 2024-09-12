5d07be150b41c8cb59ee4e6cf22abcba847b6a18
Emanuel H <emanuel.herrera38@gmail.con>
2024-09-12 15:45:55 -0300

    Se modifica clase cliente

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -2,5 +2,20 @@
 package usuarios.modelos;
 
 public class Cliente {
+    public String correo;
+    public String clave;
+    public String apellido;
+    public String nombre;
+
+    void mostrar() {   
+     
+        System.out.println("Correo:" +correo);
+        System.out.println("Clave: " +clave);
+        System.out.println("Apellido" +apellido);
+        System.out.println("Nombre:" +nombre);
+        
+    }
+
+
     
 }
