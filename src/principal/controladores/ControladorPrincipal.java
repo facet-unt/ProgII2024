3f22967f85d3ee32c8ea25c6ec3c79c194730157
Bruno-David <Bruno-David@prog.com>
2024-09-16 18:05:55 -0300

    Creación de clase ControladorPrincipal y se definen los ArrayList para cada clase

---
+++ src/principal/controladores/ControladorPrincipal.java
@@ -0,0 +1,15 @@
+package principal.controladores;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rodrigoAndres
+ */
+public class ControladorPrincipal {
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
