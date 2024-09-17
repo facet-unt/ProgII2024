7ded7dfdb17e55e59634db98d9d660459ce6ddbf
Diego Galvan <diegosebastiangalvan2018@gmail.com>
2024-09-17 17:51:53 -0300

    Diego Galvan: Encapsulamiento de la clase producto, agregado de sus metodos get/set y definicion del constructor

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -3,12 +3,22 @@ package productos.modelos;
 import usuarios.modelos.Cliente;
 
 public class Producto {
-    public int codigo;
-    public String nombre;
-    public String descripcion;
-    public float precio;
-    public String estado;
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private String estado;
+    private float precio;
 
+    // Definicion del constructor de clase
+    
+    public Producto(int codigo, String nombre, String descripcion, String estado, float precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.precio = precio;
+    }
+    
     /**
      * Este método permite mostrar un Producto
      * @return devuelve nada
@@ -21,9 +31,53 @@ public class Producto {
         System.out.println("Descripcion: " + descripcion);
         System.out.println("Estado: " + estado);
     }
-       
-    public String toString(){
+    
+    // Override del metodo toString
+    
+    public @Override String toString(){
         return descripcion;
+    }
+    
+    // Definicion de los metodos get/set para las variables de instancia
+    
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public void asignarCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String verDescripcion() {
+        return descripcion;
+    }
+
+    public void asignarDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public String verEstado() {
+        return estado;
+    }
+
+    public void asignarEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public float verPrecio() {
+        return precio;
+    }
+
+    public void asignarPrecio(float precio) {
+        this.precio = precio;
     }
 }
 
