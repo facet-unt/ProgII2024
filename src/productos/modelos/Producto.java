1c24e7926f63183c8214095165449c6b4b0cdd96
Emanuel <emanuel@facet.com>
2024-10-01 18:36:15 -0300

    Notas del parche: Se agregaron las interfaces gráficas de las clases Encargado, Empleado, Cliente, Producto y ControladorPrincipal Se agregó el código correspondiente para que funcione el código del botón guardar en las interfaces gráficas de las clases Encargado y Empleado

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -4,46 +4,72 @@ import usuarios.modelos.Cliente;
 
 public class Producto {
     private int codigo;
-    private String nombre;
     private String descripcion;
     private float precio;
-    private String estado;        
+    private String estado;
+    private String categoria;
+
+    /**
+     * Este método permite mostrar un Producto
+     * @return devuelve nada
+     */
+
     public void mostrar(){
-        System.out.println(nombre+"\n"+descripcion+"\n"+precio+"\n"+estado);
-    }    
-    @Override
-    public String toString(){
+    System.out.println("Codigo: "+ codigo + "\nDescripcion: "+ descripcion + "\nCategoria: "+ categoria + "\nEstado: "+ estado + "\nPrecio: "+ precio);
+    }
+
+    public Producto(int codigo, String descripcion, float precio, String estado, String categoria) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.estado = estado;
+        this.categoria = categoria;
+    }
+
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public void asignarCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String verDescripcion() {
         return descripcion;
     }
-    public String verNombre(){
-        return nombre;
+
+    public void asignarDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
-    public void asignarNombre(String nombre){
-        this.nombre=nombre;
-    }
-    public String verDescripcion(){
-        return descripcion;
-    }
-    public void asignarDescripcion(String descripcion){
-        this.descripcion=descripcion;
-    }
-    public float verPrecio(){
+
+    public float verPrecio() {
         return precio;
     }
-    public void asignarPrecio(float precio){
-        this.precio=precio;
+
+    public void asignarPrecio(float precio) {
+        this.precio = precio;
     }
-    public String verEstado(){
+
+    public String verEstado() {
         return estado;
     }
-    public void asignarEstado(String estado){
-        this.estado=estado;
+
+    public void asignarEstado(String estado) {
+        this.estado = estado;
     }
-    public Producto(int codigo, String nombre, String descripcion, String estado, float precio){
-        this.nombre=nombre;
-        this.descripcion=descripcion;
-        this.estado=estado;
-        this.precio=precio;
-        this.codigo=codigo;
+
+    public String verCategoria() {
+        return categoria;
     }
+
+    public void asignarCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+     
 }
+
+
+
+
