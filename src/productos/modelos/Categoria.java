37c3602fe48e0b6c641814ca49d2e19bdf1e9e18
Lisandro Rivas <gonzalonasul34@gmail.com>
2024-10-01 18:43:41 -0300

    Creacion de enumeraciones Categoria y Estado

---
+++ src/productos/modelos/Categoria.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package productos.modelos;
+
+/**
+ *
+ * @author estudiante
+ */
+public enum Categoria {
+    ENTRADA("Entrada"),
+    PLATOPRINCIPAL("Plato Principal"),
+    POSTRE("Postre");
+    
+    private String valor;
+
+    private Categoria(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return this.valor;
+    }
+    
+    
+    
+}
