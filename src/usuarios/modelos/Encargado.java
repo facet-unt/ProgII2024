92609a44cea67faaf2a79ab6e90fcac86a257aa0
Juan Pablo <juampirearte2@hotmail.com>
2024-10-08 14:30:09 -0300

    empleado y encargado

--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -9,16 +9,12 @@ package usuarios.modelos;
  * @author juamp
  */
 public class Encargado {
-    
- * @author estudiante
- */
-public class Encargado {
-    
-    public String correo;
-    public String clave;
-    public String apellido;
-    public String nombre;
-    
+
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
+
     public void mostrar() {
         System.out.println("Datos del encargado");
         System.out.println("Correo: " + correo);
@@ -26,4 +22,48 @@ public class Encargado {
         System.out.println("Apellido: " + apellido);
         System.out.println("Nombre: " + nombre);
     }
+
+    public Encargado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public Encargado(String c, String a, String n) {
+        this(c, "123466", a, n);
+    }
+
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
