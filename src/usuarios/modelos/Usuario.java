3c89ea39e033ca24bf0a3d025a2f3792ec56097d
Diego Galvan <diegosebastiangalvan2018@gmail.com>
2024-10-08 17:25:18 -0300

    Diego Galvan: Creacion de la clase usuario, y definicion de sus variables de instancia, metodos get set, y constructor

---
+++ src/usuarios/modelos/Usuario.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author estudiante
+ */
+public abstract class Usuario {
+    
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
+    
+    // Definicion del constructor de clase
+    
+    public Usuario(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+    
+    // Definicion del metodo mostrar
+    
+    public void mostrar() {
+        System.out.println("Datos del usuario");
+        System.out.println("Correo: " + correo);
+        System.out.println("Clave: " + clave);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Nombre: " + nombre);
+    }
+    
+    // Definicion de los metodos get/set de la clase para las 4 variables de instancia
+    
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
