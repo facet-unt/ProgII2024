c52529122bb22c8ffd7df93f363e3a5208712adf
Juan Pablo <juampirearte2@hotmail.com>
2024-10-08 14:29:56 -0300

    empleado y encargado

--- src/usuarios/modelos/Empleado.java
+++ src/usuarios/modelos/Empleado.java
@@ -8,7 +8,7 @@ import productos.modelos.Producto;
 
 public class Empleado {
 
-    public static void main(String[] args) {
+    /*    public static void main(String[] args) {
         Producto unProducto = new Producto();
         unProducto.mostrar();
         unProducto.nombre = "Pizza";
@@ -16,24 +16,63 @@ public class Empleado {
         unProducto.precio = 35.50f;
         unProducto.mostrar();
         System.out.println(unProducto.toString());
-    }
+    }*/
+    
+   
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
 
-/**
- *
- * @author estudiante
- */
-public class Empleado {
-    
-    public String correo;
-    public String clave;
-    public String apellido;
-    public String nombre;
-    
     public void mostrar() {
         System.out.println("Datos del empleado");
         System.out.println("Correo: " + correo);
         System.out.println("Clave: " + clave);
         System.out.println("Apellido: " + apellido);
         System.out.println("Nombre: " + nombre);
+
+    }
+
+    public Empleado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public Empleado(String c, String a, String n) {
+        this(c, "123466", a, n);
+    }
+
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
