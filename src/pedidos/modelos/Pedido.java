b0613f2c72904df9825d37d0c439bf89add39bda
martin middagh <martinmiddagh@gmail.com>
2024-10-08 15:54:53 -0300

    redefinido equals en Producto y Pedido

--- src/pedidos/modelos/Pedido.java
+++ src/pedidos/modelos/Pedido.java
@@ -74,4 +74,27 @@ public class Pedido {
                 +"Estado: "+this.estado.toString()+"\n");
     //la consigna dice que tiene que ser apellido, nombre. se podria separar el String nombre cuando aparece un espacio, pero hay que hacer trim al crear clientes, y no se que hacer con multiples nombres
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.numero;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pedido other = (Pedido) obj;
+        return this.numero == other.numero;
+    }
+    
 }
