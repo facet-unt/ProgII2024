fe9471cfceb4671de70c1ab1a5f228ac11007db6
Nicolas Arnedo <nicolasarnedo140@gmail.com>
2024-10-13 08:16:42 -0300

    Se hicieron subclase de Usuario a las clases Cliente, Empleado y Encargado.

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -4,60 +4,19 @@ package usuarios.modelos;
 import java.util.ArrayList;
 import pedidos.modelos.Pedido;
 
-public class Cliente {
+public class Cliente extends Usuario {
     
-    private String correo;
-    private String clave;
-    private String apellido;
-    private String nombre;
     private ArrayList<Pedido> pedidos = new ArrayList<>();
-    
-    public void mostrar(){
-    System.out.println("Correo: "+ correo + "\nClave: "+ clave + "\nApellido: "+ apellido + "\nNombre: "+ nombre);
-    }
 
     public Cliente(String correo, String clave, String apellido, String nombre) {
-        this.correo = correo;
-        this.clave = clave;
-        this.apellido = apellido;
-        this.nombre = nombre;
-    }
-        
-    public Cliente(String c, String a, String n) {
-        this(c, "123466", a, n);   
+        super(correo, clave, apellido, nombre);
     }
     
     
-    public String verCorreo() {
-        return correo;
-    }
 
-    public void asignarCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String verClave() {
-        return clave;
-    }
-
-    public void asignarClave(String clave) {
-        this.clave = clave;
-    }
-
-    public String verApellido() {
-        return apellido;
-    }
-
-    public void asignarApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String verNombre() {
-        return nombre;
-    }
-
-    public void asignarNombre(String nombre) {
-        this.nombre = nombre;
-    }
+      
+    
+    
+    
 
 }
