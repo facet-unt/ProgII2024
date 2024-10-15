fc544d12fd03571a84529df7a0b7b1f609debbdf
Diego Galvan <diegosebastiangalvan2018@gmail.com>
2024-10-15 17:32:52 -0300

    Diego Galvan: Modificacion del metodo equals en usuario para corregir distintos tipos de usuarios repetidos

--- src/usuarios/modelos/Usuario.java
+++ src/usuarios/modelos/Usuario.java
@@ -54,7 +54,7 @@ public abstract class Usuario {
         if (obj == null) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (getClass().getSuperclass() != obj.getClass().getSuperclass()) {
             return false;
         }
         final Usuario other = (Usuario) obj;
