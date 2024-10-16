8f1323782eafe80095ffaa82ada46dd9aa17ec38
Gisela Palomino <palominoflia4@gmail.com>
2024-10-16 13:33:40 -0300

    se crea la super clase se modifica para guardar o no datos y se cumple la herencia en las clases cliente,encargado y empleado PARTE2 DEL TP4 LISTA

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -4,70 +4,24 @@ package usuarios.modelos;
 import pedidos.modelos.Pedido;
 import java.util.ArrayList;
 
-public class Cliente {
-    
-    private String correo;
-    private String clave;
-    private String apellido;
-    private String nombre;
+public class Cliente extends Usuario{
+
     
     private ArrayList<Pedido> pedidos = new ArrayList<>();
     
+    @Override
     public void mostrar(){
-    System.out.println("Correo: "+ correo);
-    System.out.println("Clave: "+ clave); 
-    System.out.println("Cliente: "+ apellido +" "+nombre);
-        System.out.println("pedidos");
+    super.mostrar();
     for(Pedido p: pedidos)
        
             System.out.println(p.verNumero());
     }
 
     public Cliente(String correo, String clave, String apellido, String nombre) {
-        this.correo = correo;
-        this.clave = clave;
-        this.apellido = apellido;
-        this.nombre = nombre;
+       super(correo,clave,apellido,nombre);
     }
     
     public void agrgarPedido(Pedido unpedido){
         this.pedidos.add(unpedido);
-    }
-        
-    public Cliente(String c, String a, String n) {
-        this(c, "123466", a, n);   
-    }
-    
-    
-    public String verCorreo() {
-        return correo;
-    }
-
-    public void asignarCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String verClave() {
-        return clave;
-    }
-
-    public void asignarClave(String clave) {
-        this.clave = clave;
-    }
-
-    public String verApellido() {
-        return apellido;
-    }
-
-    public void asignarApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String verNombre() {
-        return nombre;
-    }
-
-    public void asignarNombre(String nombre) {
-        this.nombre = nombre;
     }
 }
--- src/usuarios/modelos/Usuario.java
+++ src/usuarios/modelos/Usuario.java
@@ -5,11 +5,13 @@
  */
 package usuarios.modelos;
 
+import java.util.Objects;
+
 /**
  *
  * @author Gisela
  */
-public class Usuario {
+public abstract class Usuario {
     
     private String correo;
     private String clave;
@@ -23,37 +25,69 @@ public class Usuario {
         this.nombre = nombre;
     }
 
-    public String getCorreo() {
+    public String verCorreo() {
         return correo;
     }
 
-    public void setCorreo(String correo) {
+    public void asignarCorreo(String correo) {
         this.correo = correo;
     }
 
-    public String getClave() {
+    public String verClave() {
         return clave;
     }
 
-    public void setClave(String clave) {
+    public void asignarClave(String clave) {
         this.clave = clave;
     }
 
-    public String getApellido() {
+    public String verApellido() {
         return apellido;
     }
 
-    public void setApellido(String apellido) {
+    public void asignarApellido(String apellido) {
         this.apellido = apellido;
     }
 
-    public String getNombre() {
+    public String verNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void asignarNombre(String nombre) {
         this.nombre = nombre;
     }
     
+     public void mostrar(){
+    System.out.println("Correo: "+ correo);
+    System.out.println("Clave: "+ clave); 
+    System.out.println("Cliente: "+ apellido +" "+nombre);
+     }
     
+     
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.correo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass().getSuperclass() != obj.getClass().getSuperclass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.correo, other.correo)) {
+            return false;
+        }
+        return true;
+    }
+     
+     
 }
