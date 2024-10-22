b3d16a7312c7be568b400f04f67877b30a33316d
Mauro Herrera <mauroh568@gmail.com>
2024-10-22 18:04:21 -0300

    Agregado del metodo Buscar Producto

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -1,8 +1,6 @@
+package productos.modelos;
 
 import java.util.ArrayList;
-import productos.modelos.Categoria;
-import productos.modelos.Estado;
-import productos.modelos.Producto;
 
 public class GestorProductos {
     private ArrayList<Producto> productos = new ArrayList<>();
@@ -10,9 +8,10 @@ public class GestorProductos {
     public static final String EXITO = "Producto creado/modificado con exito";
     public static final String ERROR_CODIGO = "El código del producto es incorrecto";
     public static final String ERROR_DESCRIPCION = "La descripción del producto es incorrecta";
+    public static final String VALIDACION_EXITO = "Los datos del producto son correctos";
+    public static final String PRODUCTO_INEXISTENTE = "No existe el producto especificado";
     
-    private GestorProductos(){
-    }
+    private GestorProductos(){}
                 
     public static GestorProductos instanciarGestorProductos(){
         if (gestor == null)
@@ -25,3 +24,19 @@ public class GestorProductos {
         Producto producto = new Producto(codigo, descripcion, categoria, estado, precio);
         
     }
+    
+    public ArrayList<Producto> buscarProductos(String descripcion){
+        
+        String descripcionEnMinuscula  = descripcion.toLowerCase();
+        ArrayList <Producto> recorrer = new ArrayList<>();
+        
+        for(Producto prod : productos){
+            if(prod.verDescripcion().equals(descripcionEnMinuscula)){
+                recorrer.add(prod);
+            }    
+            else
+                System.out.println(PRODUCTO_INEXISTENTE);
+         }
+        return recorrer;
+    }
+}
