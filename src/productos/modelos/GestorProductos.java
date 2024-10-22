c83990287417d28cf560494262c05dc9aed503de
Nicolas Arnedo <nicolasarnedo140@gmail.com>
2024-10-22 18:50:48 -0300

    Creacion de los metodos crearProducto y modificarProducto en la clase GestorProductos

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -14,6 +14,15 @@ public class GestorProductos {
     
     private ArrayList<Producto> productos = new ArrayList<>();
     private static GestorProductos gestor;
+    public static final String EXITO = "Producto creado/modificado con éxito";
+public static final String ERROR_CODIGO = "El código del producto es incorrecto";
+public static final String ERROR_DESCRIPCION = "La descripción del producto es incorrecta";
+public static final String ERROR_PRECIO = "El precio del producto es incorrecto";
+public static final String ERROR_CATEGORIA = "La categoría del producto es incorrecta";
+public static final String ERROR_ESTADO = "El precio del producto es incorrecto";
+public static final String PRODUCTOS_DUPLICADOS = "Ya existe un producto con ese código";
+public static final String VALIDACION_EXITO = "Los datos del producto son correctos";
+public static final String PRODUCTO_INEXISTENTE = "No existe el producto especificado";
 
     private GestorProductos() {
     }
@@ -24,6 +33,77 @@ public class GestorProductos {
         }
         return gestor;
     }
+    
+    public String crearProducto(int codigo, String descripcion, float precio, Categoria categoria, Estado estado){
+        if(codigo < 0){
+        return ERROR_CODIGO;
+        }
+        
+        if(descripcion  ==  null || descripcion.isEmpty()){
+        return ERROR_DESCRIPCION;
+        }
+        
+        if(precio < 0){
+            return ERROR_PRECIO;
+        }
+        
+        if(categoria == null){
+        return ERROR_CATEGORIA;
+        }
+        
+        if(estado == null){
+        return ERROR_ESTADO;
+        }
+        
+        Producto producto = new Producto(codigo, descripcion, categoria, estado, precio);
+        
+        if(productos.contains(producto)){
+        return PRODUCTOS_DUPLICADOS;
+        }
+        
+        productos.add(producto);
+        
+        return(EXITO);
+    }
+    
+    
+    
+    public String modificarProducto(Producto productoAModificar, int codigo, String descripcion, float precio, Categoria categoria, Estado estado){
+        if(codigo < 0){
+        return ERROR_CODIGO;
+        }
+        
+        if(descripcion  ==  null || descripcion.isEmpty()){
+        return ERROR_DESCRIPCION;
+        }
+        
+        if(precio < 0){
+            return ERROR_PRECIO;
+        }
+        
+        if(categoria == null){
+        return ERROR_CATEGORIA;
+        }
+        
+        if(estado == null){
+        return ERROR_ESTADO;
+        }
+        
+        if(!productos.contains(productoAModificar)){
+        return PRODUCTO_INEXISTENTE;
+        }
+        
+        int posicion = productos.indexOf(productoAModificar);
+        
+        Producto productoNuevo = new Producto(codigo, descripcion, categoria, estado, precio);
+        
+        productos.set(posicion, productoNuevo);
+        
+        return 
+        
+        
+    }
+    
     
     
     
