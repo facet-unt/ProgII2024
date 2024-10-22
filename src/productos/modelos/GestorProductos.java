c516fd7d1b3c2afe71ce0291f71f88a15ec54661
Alvaro Rojas <alvarolrojas@yahoo.com>
2024-10-22 18:17:24 -0300

    Creacion de metodos crearProducto(), modificarProducto(), e implementacion del patron single

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -1,15 +1,22 @@
+package productos.modelos;
 
 import java.util.ArrayList;
-import productos.modelos.Categoria;
-import productos.modelos.Estado;
-import productos.modelos.Producto;
 
 public class GestorProductos {
     private ArrayList<Producto> productos = new ArrayList<>();
     private static GestorProductos gestor;
+    private int bandera;
     public static final String EXITO = "Producto creado/modificado con exito";
+    public static final String ERROR_PRODUCTO ="No se pudo crear el producto";
     public static final String ERROR_CODIGO = "El código del producto es incorrecto";
     public static final String ERROR_DESCRIPCION = "La descripción del producto es incorrecta";
+    public static final String ERROR_PRECIO = "El precio del producto es incorrecto";
+    public static final String ERROR_CATEGORIA = "La categoria del producto es incorrecta";
+    public static final String ERROR_ESTADO = "El estado del producto es incorrecto";
+    public static final String PRODUCTOS_DUPLICADOS = "Ya existe un producto con ese codigo";
+    public static final String VALIDACION_EXITO = "Los datos del producto son correctos";
+    public static final String PRODUCTO_INEXISTENTE = "No existe el producto especificado";
+    
     
     private GestorProductos(){
     }
@@ -22,6 +29,39 @@ public class GestorProductos {
     }
     
     public String crearProducto(int codigo, String descripcion, float precio, Categoria categoria, Estado estado){
-        Producto producto = new Producto(codigo, descripcion, categoria, estado, precio);
-        
+        if(!(codigo>0))
+            return ERROR_CODIGO;
+        if(descripcion == null)
+            return ERROR_DESCRIPCION;
+        if(!(precio>0.0))
+            return ERROR_PRECIO;
+        if(categoria == null)
+            return ERROR_CATEGORIA;
+        if(estado == null)
+            return ERROR_ESTADO;
+        if(codigo > 0 && descripcion != null && precio > 0.0 && categoria != null && estado != null ){
+            Producto producto = new Producto(codigo, descripcion, categoria, estado, precio);
+            productos.add(producto);
+            return EXITO;
+        }
+        else
+            return ERROR_PRODUCTO;      
     }
+    public String modificarProducto(Producto productoAModificar, int codigo, String descripcion, float precio, Categoria categoria, Estado estado){
+            bandera = 0;
+            for(Producto p : productos){
+                if(p.equals(productoAModificar)){
+                    p.asignarCodigo(productoAModificar.verCodigo());
+                    p.asignarDescripcion(productoAModificar.verDescripcion());
+                    p.asignarPrecio(productoAModificar.verPrecio());
+                    p.asignarCategoria(productoAModificar.verCategoria());
+                    p.asignarEstado(productoAModificar.verEstado());
+                    bandera = 1;
+                    }
+        } 
+            if(bandera == 1)
+                return EXITO;
+            else
+                return PRODUCTO_INEXISTENTE;
+    } 
+} 
