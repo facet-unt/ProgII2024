8c72e42d5e7624e08c3df8007a3fa834c9123883
Nicolas Arnedo <nicolasarnedo140@gmail.com>
2024-10-23 14:12:42 -0300

    Creacion del metodo validador en la clase GestorUsuarios

--- src/usuarios/modelos/GestorUsuarios.java
+++ src/usuarios/modelos/GestorUsuarios.java
@@ -13,7 +13,19 @@ import java.util.ArrayList;
 public class GestorUsuarios {
     ArrayList<Usuario> usuarios = new ArrayList<>();
     private static GestorUsuarios gestor;
+    public static final String EXITO = "Usuario creado/modificado con éxito";
+    public static final String ERROR_CORREO = "El correo del usuario es incorrecto";
+    public static final String ERROR_APELLIDO = "El apellido del usuario es incorrecto";
+    public static final String ERROR_NOMBRE = "El nombre del usuario es incorrecto";
+    public static final String ERROR_CLAVES = "Las claves especificadas no coinciden o son incorrectas";
+    public static final String ERROR_PERFIL = "El perfil del usuario es incorrecto";
+    public static final String USUARIOS_DUPLICADOS = "Ya existe un usuario con ese correo";
+    public static final String VALIDACION_EXITO = "Los datos del usuario son correctos";
+    
 
+    
+    
+    
     private GestorUsuarios() {
     }
     
@@ -23,6 +35,26 @@ public class GestorUsuarios {
             gestor = new GestorUsuarios();
         }
         return gestor;
+    }
+    
+    private boolean validador(String correo, String apellido, String nombre, Perfil perfil, String clave, String claveRepetida){
+        if(correo == null || !correo.contains("@")){
+            return false;
+        }
+        if(apellido == null || apellido.isBlank()){
+            return false;
+        }
+        if(nombre == null || nombre.isBlank()){
+            return false;
+        }
+        if(clave == null || clave.isBlank()){
+            return false;
+        }
+        if(claveRepetida == null || claveRepetida.isBlank() || !clave.equals(claveRepetida)){
+            return false;
+        }
+        
+        return true;
     }
     
     
