a395070f1b8b45c6d9596e40c25e9822bee99076
Nicolas Arnedo <nicolasarnedo140@gmail.com>
2024-10-23 18:02:03 -0300

    Creacion de la clase GestorPedidos tal que solo puede ser instanciado 1 vez

---
+++ src/pedidos/modelos/GestorPedidos.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pedidos.modelos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mi buen PC
+ */
+public class GestorPedidos {
+    private ArrayList<Pedido> pedidos = new ArrayList<>();
+    private static GestorPedidos gestor;
+    public static final String EXITO = "Pedido creado/modificado/cancelado con éxito";
+    public static final String ERROR_FECHA = "La fecha del pedido es incorrecta";
+    public static final String ERROR_HORA = "La hora del pedido es incorrecta";
+    public static final String ERROR_PRODUCTOS_DEL_PEDIDO = "El pedido no tiene productos";
+    public static final String ERROR_CLIENTE = "El pedido no tiene un cliente";
+    public static final String ERROR_ESTADO = "El pedido no tiene un estado";
+    public static final String ERROR_CANCELAR = "No se puede cancelar el pedido en este estado";
+    public static final String PEDIDOS_DUPLICADOS = "Ya existe un pedido con ese número";
+    public static final String PEDIDO_INEXISTENTE = "No existe el pedido especificado";
+    public static final String VALIDACION_EXITO = "El pedido tiene los datos correctos";
+
+    private GestorPedidos() {
+    }
+    
+    public static GestorPedidos instanciar(){
+        if(gestor == null){
+            gestor = new GestorPedidos();
+        }
+        return gestor;
+    }
+
+    
+    
+    
+    
+    
+}
